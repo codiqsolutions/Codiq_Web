f95f3b1c031f8bee7e8a9098059b47bafb9563ba
VasuAntala <vasuantala8283@gmail.com>
2026-02-19 18:58:32 +0530

    final_render

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,16 @@
 import type { NextConfig } from "next";
 
+const backendUrl = process.env.BACKEND_URL
+  ? (process.env.BACKEND_URL.startsWith('http') ? process.env.BACKEND_URL : `https://${process.env.BACKEND_URL}`)
+  : 'http://localhost:4000';
+
 const nextConfig: NextConfig = {
   /* config options here */
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.BACKEND_URL ? (process.env.BACKEND_URL.startsWith('http') ? process.env.BACKEND_URL : `https://${process.env.BACKEND_URL}`) : 'http://localhost:4000'}/api/:path*`, // Proxy to Backend
+        destination: `${backendUrl}/api/:path*`, // Proxy to Backend
       },
     ];
   },

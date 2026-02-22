e821db1f58240e49631f9c9307bb22e0dca7b768
VasuAntala <vasuantala8283@gmail.com>
2026-02-22 14:32:25 +0530

    fix ccss

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,14 +1,6 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {
-      base: __dirname,
-    },
+    "@tailwindcss/postcss": {},
   },
 };
 
